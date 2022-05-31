9e0f360efe51b2526e1e1a472ef4b641d7900849
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-05-31 15:55:47 +0500

    auth middleware added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controller/userController");
-
+const { registerUser, authUser } = require("../controller/userController");
+// const connectDB = require("../config/db");
+// const dotenv = require("dotenv");
+// dotenv.config();
+// connectDB();
 router.route("/").post(registerUser);
-// router.post("/login", authUser);
+router.post("/login", authUser);
 
 module.exports = router;
